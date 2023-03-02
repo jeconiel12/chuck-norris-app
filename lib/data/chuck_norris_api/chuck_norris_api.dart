01b7e11a8cad94ce2afe8532ed4650d9ae8bc802
jeconiel12 <jecojason@gmail.com>
2023-03-02 15:20:46 +0700

    chore: create chuck norris api client

---
+++ lib/data/chuck_norris_api/chuck_norris_api.dart
@@ -0,0 +1,3 @@
+export 'src/chuck_norris_api_client.dart';
+export 'src/models/models.dart';
+export 'src/responses/responses.dart';
---
+++ lib/data/chuck_norris_api/src/models/joke/joke.freezed.dart
@@ -0,0 +1,169 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'joke.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Joke _$JokeFromJson(Map<String, dynamic> json) {
+  return _Joke.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Joke {
+  @HiveField(0)
+  String get id => throw _privateConstructorUsedError;
+  @HiveField(1)
+  String get value => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $JokeCopyWith<Joke> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $JokeCopyWith<$Res> {
+  factory $JokeCopyWith(Joke value, $Res Function(Joke) then) =
+      _$JokeCopyWithImpl<$Res, Joke>;
+  @useResult
+  $Res call({@HiveField(0) String id, @HiveField(1) String value});
+}
+
+/// @nodoc
+class _$JokeCopyWithImpl<$Res, $Val extends Joke>
+    implements $JokeCopyWith<$Res> {
+  _$JokeCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? value = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      value: null == value
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_JokeCopyWith<$Res> implements $JokeCopyWith<$Res> {
+  factory _$$_JokeCopyWith(_$_Joke value, $Res Function(_$_Joke) then) =
+      __$$_JokeCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({@HiveField(0) String id, @HiveField(1) String value});
+}
+
+/// @nodoc
+class __$$_JokeCopyWithImpl<$Res> extends _$JokeCopyWithImpl<$Res, _$_Joke>
+    implements _$$_JokeCopyWith<$Res> {
+  __$$_JokeCopyWithImpl(_$_Joke _value, $Res Function(_$_Joke) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? value = null,
+  }) {
+    return _then(_$_Joke(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      value: null == value
+          ? _value.value
+          : value // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+@HiveType(typeId: 0)
+class _$_Joke extends _Joke {
+  _$_Joke({@HiveField(0) required this.id, @HiveField(1) required this.value})
+      : super._();
+
+  factory _$_Joke.fromJson(Map<String, dynamic> json) => _$$_JokeFromJson(json);
+
+  @override
+  @HiveField(0)
+  final String id;
+  @override
+  @HiveField(1)
+  final String value;
+
+  @override
+  String toString() {
+    return 'Joke(id: $id, value: $value)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Joke &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.value, value) || other.value == value));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, value);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_JokeCopyWith<_$_Joke> get copyWith =>
+      __$$_JokeCopyWithImpl<_$_Joke>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_JokeToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Joke extends Joke {
+  factory _Joke(
+      {@HiveField(0) required final String id,
+      @HiveField(1) required final String value}) = _$_Joke;
+  _Joke._() : super._();
+
+  factory _Joke.fromJson(Map<String, dynamic> json) = _$_Joke.fromJson;
+
+  @override
+  @HiveField(0)
+  String get id;
+  @override
+  @HiveField(1)
+  String get value;
+  @override
+  @JsonKey(ignore: true)
+  _$$_JokeCopyWith<_$_Joke> get copyWith => throw _privateConstructorUsedError;
+}
